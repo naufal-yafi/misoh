236bf772de6c870ab2529ad383a6fb433444e9bb
naufal-yafi <muhammadnaufalyafi00@gmail.com>
2023-07-30 07:23:50 +0700

    bundler system

---
+++ rollup.config.js
@@ -0,0 +1,19 @@
+function dateFormat() {
+    const currentDate = new Date();
+
+    const year = currentDate.getFullYear().toString().padStart(4, '0');
+    const month = (currentDate.getMonth() + 1).toString().padStart(2, '0');
+    const day = currentDate.getDate().toString().padStart(2, '0');
+    const hours = currentDate.getHours().toString().padStart(2, '0');
+    const minutes = currentDate.getMinutes().toString().padStart(2, '0');
+  
+    return `${year}${month}${day}-${hours}${minutes}`;
+}
+
+export default {
+    input: "./build/main.js",
+    output: {
+        file: `./dist/misoh-${dateFormat()}.bundle.js`,
+        format: "cjs"
+    }
+}
