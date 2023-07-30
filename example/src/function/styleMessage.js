07549fc11edd29502707482242443e528237f0c3
naufal-yafi <muhammadnaufalyafi00@gmail.com>
2023-07-30 15:31:29 +0700

    add function mark on number

--- example/src/function/styleMessage.js
+++ example/src/function/styleMessage.js
@@ -1,16 +1,18 @@
 const styleMessage = (input) => {
     const split = input.split(" ");
-    let result = ''; // Change result to be a string instead of an array
+    let result = ''; 
 
     for (let i = 0; i < split.length; i++) {
         if (split[i].includes('*')) {
+            result += `<span class="bg-green-500 text-black">${split[i]}</span>`;
+        } else if (!isNaN(parseFloat(split[i]))) {
             result += `<span class="bg-green-500 text-black">${split[i]}</span>`;
         } else {
             result += split[i];
         }
 
         if (i !== split.length - 1) {
-            result += ' '; // Add space between words
+            result += ' ';
         }
     }
 
