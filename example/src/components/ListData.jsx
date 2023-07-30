a4a5cc3ddf5ae7ae2045ea275bfb1cfe849883e7
naufal-yafi <muhammadnaufalyafi00@gmail.com>
2023-07-30 15:45:50 +0700

    setup deploy workflow

--- example/src/components/ListData.jsx
+++ example/src/components/ListData.jsx
@@ -2,12 +2,10 @@ const ListData = ({messageList}) => {
     return (
         <>
         { messageList.map((message, index) => (
-          <p id="message-bar" className={`text-${message.color} text-sm`} key={index}> 
-            <div className="flex flex-row gap-2">
+          <section id="message-bar" className={`text-${message.color} text-sm flex flex-row gap-2`} key={index}> 
               <div>[{message.date}]</div>
               <div dangerouslySetInnerHTML={{ __html: message.text }}></div>
-            </div>
-          </p>
+          </section>
         ))}
         </>
     )
